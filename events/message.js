74d66020dd6d462dc25adfdfb56a4125b1a6e8a5
AKSalter <Andrew.k.salter@gmail.com>
2019-03-10 20:45:51 -0700

    refactoring

--- events/message.js
+++ events/message.js
@@ -1,9 +1,6 @@
 const kick = require('../commands/kick')
 
 module.exports = (client, message) => {
-  if (message.content === `${prefix}ping`) {
-    message.reply('Pong!')
-  }
   if (message.content.startsWith(`${prefix}kick`)) {
 	  return kick(message)
   }
