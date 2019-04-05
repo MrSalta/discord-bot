22c6a236f1118766d51e6c4c52c712c0fdeff0e0
Andrew Salter <andrew.k.salter@disney.com>
2019-04-04 20:58:43 -0700

    Got somewhere!

--- commands/menu.js
+++ commands/menu.js
@@ -11,84 +11,28 @@ var reaction_numbers = [
   "\u0038\u20E3",
   "\u0039\u20E3"]
 
-const pages = {
-  1: {
-    title: "Test Title",
-    color: 0x253b56,
-    author: {
-      name: 'Some name',
-      icon_url: 'https://cdn.discordapp.com/embed/avatars/0.png',
-      url: 'https://discordapp.com',
-    },
-    description: "Random Stuff",
-    footer: {
-      text: "More stuff",
-      icon_url: "https://discord.net",
-    },
-    image: {
-      url: "https://cdn.discordapp.com/embed/avatars/0.png",
-    },
-    thumbnail: {
-      url: "https://cdn.discordapp.com/embed/avatars/0.png",
-    },
-    timestamp: new Date(),
-    //.setURL("if you want one")
-    fields: [
-      {
-        name: "1️⃣ Destiny 2",
-        value: "---",
-      },
-      {
-        name: "2️⃣ Overwatch",
-        value: "---",
-      },
-      {
-        name: "3️⃣ Apex Legends",
-        value: "---",
-      },
-      {
-        name: "4️⃣ Anthem",
-        value: "---",
-      }
-    ]
-  },
-  2: { title: 'Destiny 2', description: 'Destiny 2 Options' }
-};
-
-const removeReaction = async (m, message, emoji) => {
-  try { m.reactions.find(r => r.emoji.name == emoji).users.remove(message.author.id); }
-  catch (err) { }
-}
-
-const awaitReactions = async (message, m, options, filter) => {
-  const { min, max, page, limit } = options;
-
-  m.awaitReactions(filter, {
-    max: 1, time: 60000, errors: ['time']
-  })
-    .then(async (collected) => {
-      const reaction = collected.first();
-
-      if (reaction.emoji.name === "\u0031\u20E3") {
-        await removeReaction(m, message, "\u0031\u20E3");
-
-        if (page != max) {
-          page = page + 1;
-          await m.edit({ embed: pages[page] });
-        }
-        awaitReactions(msg, m, options, filter);
-      }
-    })
-
-    .catch(collected => {
-      console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
-      message.reply('No reactions in time');
-    });
-}
 module.exports = {
   name: 'menu',
   description: 'Testing Menu Stuff',
   async execute(message) {
+    let pages = ['Game Menu', 'Destiny 2 Main', 'Overwatch Main', 'Apex Main', 'Anthem Main'];
+    let page = 1;
+
+    const exampleEmbed = new Discord.RichEmbed()
+      .setColor('#0099ff')
+      .setTitle('Some title')
+      .setURL('https://discordapp.com')
+      .setDescription(pages[page - 1])
+      .setThumbnail('https://cdn.discordapp.com/embed/avatars/0.png')
+      .addField('Regular field title', 'Some value here')
+      .addBlankField()
+      .addField('1️⃣ Destiny 2', '--', true)
+      .addField('2️⃣ Overwatch', '--', true)
+      .addField('3️⃣ Apex Legends', '--', true)
+      .addField('4️⃣ Anthem', '--', true)
+      .setImage('https://cdn.discordapp.com/embed/avatars/0.png')
+      .setTimestamp()
+      .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
 
     //const options = {
     // limit: 15 * 1000,
@@ -97,24 +41,37 @@ module.exports = {
     // page: 1
     //  };
 
-    const m = await message.channel.send({ embed: pages[options.page] });
+    const m = await message.channel.send(exampleEmbed);
     await m.react(reaction_numbers[1]);
     await m.react(reaction_numbers[2]);
     await m.react(reaction_numbers[3]);
     await m.react(reaction_numbers[4]);
     await m.react("❌");
 
-    const filter = (reaction) => {
+    const filter = (reaction, user) => {
       return [
         reaction_numbers[1],
         reaction_numbers[2],
         reaction_numbers[3],
         reaction_numbers[4],
         "❌"
-      ].includes(reaction.emoji.name);
+      ].includes(reaction.emoji.name) && user.id === message.author.id;
     };
 
-    awaitReactions(message, m, options, filter);
+    m.awaitReactions(filter, {
+      max: 1, time: 60000, errors: ['time']
+    })
+      .then(collected => {
+        const reaction = collected.first();
+
+        if (reaction.emoji.name === "\u0031\u20E3") {
+          reaction.remove(reaction.users.filter(u => u === message.author).first());
+          message.reply('You chose Destiny 2');
+        }
+      })
+      .catch(collected => {
+        console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
+      })
 
 
     //reaction.emoji.name === "2️⃣" ||
