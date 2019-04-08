4eedfe09cb08aa62d3f8d342d9fcbb097440223e
Andrew Salter <andrew.k.salter@disney.com>
2019-04-07 18:33:55 -0700

    DID IT!

--- commands/menu.js
+++ commands/menu.js
@@ -11,43 +11,77 @@ var reaction_numbers = [
   "\u0038\u20E3",
   "\u0039\u20E3"]
 
-var pages = [{
-  color: '#253b56',
-  title: 'Page 1 Title',
-  url: 'https://discord.app.com',
-  thumbnail: 'https://cdn.discordapp.com/embed/avatars/0.png',
-  fieldTitle: ('Regular field title', 'Some Value Here'),
-  field1: ('1️⃣ Destiny 2', '--', true),
-  field2: ('2️⃣ Overwatch', '--', true),
-  field3: ('3️⃣ Apex Legends', '--', true),
-  field4: ('4️⃣ Anthem', '--', true),
-  image: 'https://cdn.discordapp.com/embed/avatars/0.png',
-}
-]
+const gameMenu = new Discord.RichEmbed()
+  .setColor('#253b56')
+  .setTitle('Game Menu')
+  .setURL('http://www.example.com')
+  .setDescription('description')
+  .setThumbnail('https://cdn.discordapp.com/embed/avatars/0.png')
+  .addField('Regular field title', 'Some value here')
+  .addBlankField()
+  .addField('1️⃣ Destiny 2', '--', true)
+  .addField('2️⃣ Overwatch', '--', true)
+  .addField('3️⃣ Apex Legends', '--', true)
+  .addField('4️⃣ Anthem', '--', true)
+  .setImage('https://cdn.discordapp.com/embed/avatars/0.png')
+  .setTimestamp()
+  .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
+
+const destinyMenu = new Discord.RichEmbed()
+  .setColor('#346f41')
+  .setTitle('Destiny Menu')
+  .setURL('http://www.example.com')
+  .setDescription('d2 description')
+  .setThumbnail('https://alternative.me/icons2/destiny.png')
+  .addField('Regular field title', 'Some value here')
+  .addBlankField()
+  .addField('1️⃣ Destiny 2', '--', true)
+  .addField('2️⃣ Overwatch', '--', true)
+  .addField('3️⃣ Apex Legends', '--', true)
+  .addField('4️⃣ Anthem', '--', true)
+  .setImage('https://cdn.discordapp.com/embed/avatars/0.png')
+  .setTimestamp()
+  .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
+
+const activityMenu = new Discord.RichEmbed()
+  .setColor('#253b56')
+  .setTitle('Activity Menu')
+  .setURL('example.com')
+  .setDescription('description')
+  .setThumbnail('https://cdn.discordapp.com/embed/avatars/0.png')
+  .addField('Regular field title', 'Some value here')
+  .addBlankField()
+  .addField('1️⃣ Destiny 2', '--', true)
+  .addField('2️⃣ Overwatch', '--', true)
+  .addField('3️⃣ Apex Legends', '--', true)
+  .addField('4️⃣ Anthem', '--', true)
+  .setImage('https://cdn.discordapp.com/embed/avatars/0.png')
+  .setTimestamp()
+  .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
+
+const miscMenu = new Discord.RichEmbed()
+  .setColor('#253b56')
+  .setTitle('Misc Menu')
+  .setURL('example.com')
+  .setDescription('description')
+  .setThumbnail('https://cdn.discordapp.com/embed/avatars/0.png')
+  .addField('Regular field title', 'Some value here')
+  .addBlankField()
+  .addField('1️⃣ Destiny 2', '--', true)
+  .addField('2️⃣ Overwatch', '--', true)
+  .addField('3️⃣ Apex Legends', '--', true)
+  .addField('4️⃣ Anthem', '--', true)
+  .setImage('https://cdn.discordapp.com/embed/avatars/0.png')
+  .setTimestamp()
+  .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
+
 
 module.exports = {
   name: 'menu',
   description: 'Testing Menu Stuff',
   async execute(message) {
 
-    let page = 1;
 
-    const exampleEmbed = new Discord.RichEmbed(page)
-      .setColor(pages[page].color)
-      .setTitle(pages[page].title)
-      .setURL(pages[page].url)
-      .setDescription(pages[page - 1])
-      .setThumbnail(pages[page].thumbnail)
-      .addField('Regular field title', 'Some value here')
-      .addBlankField()
-      .addField('1️⃣ Destiny 2', '--', true)
-      .addField('2️⃣ Overwatch', '--', true)
-      .addField('3️⃣ Apex Legends', '--', true)
-      .addField('4️⃣ Anthem', '--', true)
-      .setImage('https://cdn.discordapp.com/embed/avatars/0.png')
-      .setTimestamp()
-      .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
-    console.log('example embed posted');
     //const options = {
     // limit: 15 * 1000,
     // min: 1,
@@ -55,7 +89,7 @@ module.exports = {
     // page: 1
     //  };
 
-    const m = await message.channel.send(exampleEmbed[page]);
+    const m = await message.channel.send(gameMenu);
     await m.react(reaction_numbers[1]);
     await m.react(reaction_numbers[2]);
     await m.react(reaction_numbers[3]);
@@ -82,6 +116,7 @@ module.exports = {
           reaction.remove(reaction.users.filter(u => u === message.author).first());
           message.reply('You chose Destiny 2');
           console.log(`${message.author} chose Destiny 2`);
+          m.edit(destinyMenu);
         }
       })
       .catch(collected => {
