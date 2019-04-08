5981e38e8056b7cb679d176e6c89833adcecc74f
Andrew Salter <andrew.k.salter@disney.com>
2019-04-08 09:05:26 -0700

    trying to figure out delete

--- commands/menu.js
+++ commands/menu.js
@@ -27,67 +27,10 @@ const gameMenu = new Discord.RichEmbed()
   .setTimestamp()
   .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
 
-const destinyMenu = new Discord.RichEmbed()
-  .setColor('#346f41')
-  .setTitle('Destiny Menu')
-  .setURL('http://www.example.com')
-  .setDescription('d2 description')
-  .setThumbnail('https://alternative.me/icons2/destiny.png')
-  .addField('Regular field title', 'Some value here')
-  .addBlankField()
-  .addField('1️⃣ Destiny 2', '--', true)
-  .addField('2️⃣ Overwatch', '--', true)
-  .addField('3️⃣ Apex Legends', '--', true)
-  .addField('4️⃣ Anthem', '--', true)
-  .setImage('https://cdn.discordapp.com/embed/avatars/0.png')
-  .setTimestamp()
-  .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
-
-const activityMenu = new Discord.RichEmbed()
-  .setColor('#253b56')
-  .setTitle('Activity Menu')
-  .setURL('example.com')
-  .setDescription('description')
-  .setThumbnail('https://cdn.discordapp.com/embed/avatars/0.png')
-  .addField('Regular field title', 'Some value here')
-  .addBlankField()
-  .addField('1️⃣ Destiny 2', '--', true)
-  .addField('2️⃣ Overwatch', '--', true)
-  .addField('3️⃣ Apex Legends', '--', true)
-  .addField('4️⃣ Anthem', '--', true)
-  .setImage('https://cdn.discordapp.com/embed/avatars/0.png')
-  .setTimestamp()
-  .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
-
-const miscMenu = new Discord.RichEmbed()
-  .setColor('#253b56')
-  .setTitle('Misc Menu')
-  .setURL('example.com')
-  .setDescription('description')
-  .setThumbnail('https://cdn.discordapp.com/embed/avatars/0.png')
-  .addField('Regular field title', 'Some value here')
-  .addBlankField()
-  .addField('1️⃣ Destiny 2', '--', true)
-  .addField('2️⃣ Overwatch', '--', true)
-  .addField('3️⃣ Apex Legends', '--', true)
-  .addField('4️⃣ Anthem', '--', true)
-  .setImage('https://cdn.discordapp.com/embed/avatars/0.png')
-  .setTimestamp()
-  .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
-
-
 module.exports = {
   name: 'menu',
   description: 'Testing Menu Stuff',
   async execute(message) {
-
-
-    //const options = {
-    // limit: 15 * 1000,
-    // min: 1,
-    //  max: 4,
-    // page: 1
-    //  };
 
     const m = await message.channel.send(gameMenu);
     await m.react(reaction_numbers[1]);
@@ -113,19 +56,13 @@ module.exports = {
         const reaction = collected.first();
 
         if (reaction.emoji.name === "\u0031\u20E3") {
-          reaction.remove(reaction.users.filter(u => u === message.author).first());
-          message.reply('You chose Destiny 2');
-          console.log(`${message.author} chose Destiny 2`);
+          channel.fetchMessage(gameMenu).then(msg => msg.delete())
+            .then(msg => console.log(`${msg.author.username} chose Destiny 2`))
+
         }
       })
       .catch(collected => {
         console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
-      })
-
-
-    //reaction.emoji.name === "2️⃣" ||
-    // reaction.emoji.name === "3️⃣" ||
-    //  reaction.emoji.name === "4️⃣" ||
-    // reaction.emoji.name === "❌"
+      });
   }
 };
