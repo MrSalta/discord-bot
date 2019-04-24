664d7a43d2fbf6a4fb1d0db0028f844e6187910e
Andrew Salter <andrew.k.salter@disney.com>
2019-04-23 19:01:12 -0700

    committing and new linting

--- commands/menu.js
+++ commands/menu.js
@@ -1,22 +1,20 @@
 const Discord = require('discord.js');
-var emCh
-var m
-var gameChoice
+let gameChoice;
 
-//Declare reaction emojis so I don't have to remember their unicode values
-var reaction_numbers = [
-  "\u0030\u20E3",
-  "\u0031\u20E3",
-  "\u0032\u20E3",
-  "\u0033\u20E3",
-  "\u0034\u20E3",
-  "\u0035\u20E3",
-  "\u0036\u20E3",
-  "\u0037\u20E3",
-  "\u0038\u20E3",
-  "\u0039\u20E3"]
+// Declare reaction emojis so I don't have to remember their unicode values
+const reaction_numbers = [
+  '\u0030\u20E3',
+  '\u0031\u20E3',
+  '\u0032\u20E3',
+  '\u0033\u20E3',
+  '\u0034\u20E3',
+  '\u0035\u20E3',
+  '\u0036\u20E3',
+  '\u0037\u20E3',
+  '\u0038\u20E3',
+  '\u0039\u20E3'];
 
-//Top Menu (Choose your game)
+// Top Menu (Choose your game)
 const gameMenu = new Discord.RichEmbed()
   .setColor('#253b56')
   .setTitle('Game Menu')
@@ -33,7 +31,7 @@ const gameMenu = new Discord.RichEmbed()
   .setTimestamp()
   .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
 
-//Menu 2 - Destiny 2
+// Menu 2 - Destiny 2
 const destinyMenu = new Discord.RichEmbed()
   .setColor('#346f41')
   .setTitle('Destiny Menu')
@@ -50,23 +48,27 @@ const destinyMenu = new Discord.RichEmbed()
   .setTimestamp()
   .setFooter('Some footer text here', 'https://cdn.discordapp.com/embed/avatars/0.png');
 
-//How to make a Discord channel
+// How to make a Discord channel
 function makeChannel(message) {
-  message.guild.createChannel('event-maker', 'text')
+  message.guild.createChannel('event-maker', {
+    type: 'text',
+    options: [{
+      parent_id: '569022073133989900'
+    }]
+  })
     .then(channel => {
-      //Then put it in the right group
-      let category = message.guild.channels.find(c => c.name == "event-channels" && c.type == "category")
-      channel.setParent(category.id)
-        .then(() =>
-          //Then apply the group permissions  
-          channel.lockPermissions()
-            .then(() => {
-              console.log(`Permissions syncd! Also that string: ${channel.id} `)
-            })
-            .catch(console.error)
-        )
+      // Then put it in the right group
+      // const category = message.guild.channels.find(c => c.name == 'event-channels' && c.type == 'category');
+      // channel.setParent(category.id)
+      // .then(() =>
+      // Then apply the group permissions
+      channel.lockPermissions()
+        .then(() => {
+          console.log(`Permissions syncd! Also that string: ${channel.id} `);
+        })
+        .catch(console.error)
         .catch(console.error);
-      channel.send('Channel active')
+      channel.send('Channel active');
       return channel;
     })
     .catch(console.error);
@@ -77,62 +79,62 @@ module.exports = {
   description: 'Testing Menu Stuff',
   async execute(message) {
 
-    //Delete the command message
+    // Delete the command message
     message.delete();
 
-    //Create the channel
-    makeChannel(message)
-    var newCh = makeChannel()
+    // Create the channel
+    makeChannel(message);
+    const newCh = makeChannel();
 
-    //Send the menu to the new channel
+    // Send the menu to the new channel
     try {
-      const m = await message.guild.channels.find("id", `"${newCh}"`).send(gameMenu);
+      const m = await message.guild.channels.find('id', `"${newCh}"`).send(gameMenu);
 
-      //Add reactions to make the menu
+      // Add reactions to make the menu
       await m.react(reaction_numbers[1]);
       await m.react(reaction_numbers[2]);
       await m.react(reaction_numbers[3]);
       await m.react(reaction_numbers[4]);
-      await m.react("❌");
+      await m.react('❌');
       console.log(`Reactions set for gameMenu. Also, ${newCh}`);
 
-      //filter reactions to only look for the numbers and the user
+      // filter reactions to only look for the numbers and the user
       const filter = (reaction, user) => {
         return [
           reaction_numbers[1],
           reaction_numbers[2],
           reaction_numbers[3],
           reaction_numbers[4],
-          "❌"
+          '❌',
         ].includes(reaction.emoji.name) && user.id === message.author.id;
       };
 
       // Start looking for reactions
       m.awaitReactions(filter, {
-        max: 1, time: 60000, errors: ['time']
+        max: 1, time: 60000, errors: ['time'],
       })
         .then(collected => {
           const reaction = collected.first();
 
-          //Destiny 2 Selected
-          if (reaction.emoji.name === "\u0031\u20E3") {
+          // Destiny 2 Selected
+          if (reaction.emoji.name === '\u0031\u20E3') {
             reaction.remove(reaction.users.filter(u => u === message.author).first());
-            let gameChoice = "Destiny 2"
-            message.guild.channels.find("name", "event-maker").send('Destiny 2 Selected')
-            console.log(`${message.author.username} chose Destiny 2`)
+            gameChoice = 'Destiny 2';
+            message.guild.channels.find('name', 'event-maker').send('Destiny 2 Selected');
+            console.log(`${message.author.username} chose ${gameChoice}`);
             m.edit(destinyMenu);
 
           }
         })
         .catch(collected => {
 
-          //Timeout message
+          // Timeout message
           console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
         });
     }
     catch (error) {
-      console.log(error)
+      console.log(error);
     }
 
-  }
-}
+  },
+};
